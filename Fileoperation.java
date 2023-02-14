6612ffe60d5e5698d485ed216df789efad10e22e
20a91a0578 <84329902+20a91a0578@users.noreply.github.com>
2023-02-14 10:12:16 +0530

    Add files via upload

---
+++ Fileoperation.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Fileoperation {
+   public static void main(String[] args) {
+  /*  //writing content into file
+  ===========================================================
+    try{
+FileWriter wr=new FileWriter("newFile.txt");
+wr.write("we are adding content into the file \n");
+wr.close();
+    }
+    catch(Exception e){
+System.out.println("Erroroccured while accessing file:");
+    }
+  ===========================================================
+    */
+
+/*//to  read the content from the file
+try{
+    File myobj=new  File("newFile.txt");
+    myobj.createNewFile();
+    Scanner sc=new Scanner(myobj);
+    while(sc.hasNextLine())
+    {
+        String line=sc.nextLine();
+        System.out.println(line);
+    }
+    sc.close();
+}
+catch(Exception e)
+{
+    e.printStackTrace();
+}
+   } */
+
+}
+}
